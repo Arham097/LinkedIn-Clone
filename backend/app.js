dff5a0eb1547c7ced1f4044b1f59f0937fca68c7
Arham097 <arhamhasan70@gmail.com>
2024-09-15 12:54:29 +0500

    backend setup updated

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,13 @@ const userRouter = require('./Routes/userRoutes');
 const postRouter = require('./Routes/postRoutes');
 const notificationRouter = require('./Routes/notificationRoutes');
 const connectionRouter = require('./Routes/connectionRoutes');
+const cors = require('cors')
 
-app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
